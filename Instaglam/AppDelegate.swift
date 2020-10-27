2fe46092d3172a9a90d909010b6359d8a9742100
Zachary Yarost <yarostza@msu.edu>
2020-10-27 16:26:40 -0400

    added in the first two feed view controller and login view controller. Hooked the login stuff unto the parse dashboard.

--- Instaglam/AppDelegate.swift
+++ Instaglam/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +14,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "8RvXU9cuIgJ8ABgUfn9PPZlA7jHfvfuMrux6Xifn" // <- UPDATE
+                $0.clientKey = "qDV9bPz4wVHDDh4yGwnvROYNWBAPUxJydYUdc9cZ" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
+
+
         return true
     }
 
